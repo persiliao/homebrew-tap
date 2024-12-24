f6758df941273d58d47ab440545ef5d35e3ab24e
Persi.Liao <xiangchu.liao@gmail.com>
2024-12-24 17:00:50 +0800

    feat: brew-services-manage upgrade to 1.0.1

--- Casks/brew-services-manage.rb
+++ Casks/brew-services-manage.rb
@@ -1,9 +1,9 @@
 cask "brew-services-manage" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.0.0"
-  sha256 arm:   "f0ee55752888e80af8079754c2eea078a2233fe1f80256208cb085f5c6505643",
-         intel: "15b6f1c6c2051f8fe88b2fdadc99cc8750e3fb7de467cbdb76d3a1cdc49aee48"
+  version "1.0.1"
+  sha256 arm:   "b88ebe997e78e9ef069e15a5e1cc9fbafc081264feac5a20b023d1eb6cc9a390",
+         intel: "15efb735c97df549c74f4220f884033bb7f236fe4a72fb78dbbf04a0200e23aa"
   
   url "https://github.com/persiliao/brew-services-manage/releases/download/#{version}/Brew.Services.Manage_#{version}_#{arch}.dmg", 
     verified: "github.com/persiliao/brew-services-manage/"
